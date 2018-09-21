d0e6802cd51d2ac7fda90c8a885b697b40d6f4c1
Adam Foerster <adam@hariken.co>
2018-09-21 10:22:28 -0300

    feat: new bible icon, service workers

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDN9PKe08IDHnL5ea_ZIw9SQWPsGPot1xc',
+    authDomain: 'de-um-tempo.firebaseapp.com',
+    databaseURL: 'https://de-um-tempo.firebaseio.com',
+    projectId: 'de-um-tempo',
+    storageBucket: 'de-um-tempo.appspot.com',
+    messagingSenderId: '475759243077'
+  },
+  bible: {
+    host: 'https://getbible.net/json?v=almeida&p='
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
